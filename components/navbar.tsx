f6a424785fa2d06ca003437878287c80a0920f29
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-06-17 08:53:20 +0530

    close navbar after clicking on page link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -82,6 +82,7 @@ export default function Navbar() {
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </Link>
