33c3b9dfc8bac56a947c55ddb30dd593b76c32a2
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-06-19 14:53:44 +0530

    open links on home page (gform and discord) in a new tab

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,16 @@ export default function Home() {
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
+            target="_blank"
+            rel="noopener noreferrer"
             href={registerFormURL}
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             register now
           </a>
           <a
+            target="_blank"
+            rel="noopener noreferrer"
             href="https://discord.com/invite/yY85QRp4QS"
             className="text-sm font-semibold leading-6 text-gray-900"
           >
