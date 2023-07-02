2876a1e9e14a31ed084d4d5cf5081ba69e6178ed
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-07-02 17:22:14 +0530

    make footer color even

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ import { Separator } from "./ui/separator";
 export function Footer() {
   return (
     <footer className="min-w-full bg-gray-100">
-      <div className="text-sm w-full mx-auto max-w-full lg:max-w-7xl p-4 flex justify-center items-center flex-col sm:flex-row md:items-center sm:justify-between">
-        <span className="text-gray-500 sm:text-center dark:text-gray-400">
+      <div className="text-gray-500  dark:text-gray-400 text-sm w-full mx-auto max-w-full lg:max-w-7xl p-4 flex justify-center items-center flex-col gap-1 sm:flex-row md:items-center sm:justify-between">
+        <span className=" sm:text-center ">
           © 2023{" "}
           <a href="https://vanavani.iitm.ac.in/" className="hover:underline">
             Vana Vani
